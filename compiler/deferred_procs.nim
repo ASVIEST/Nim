a91b2c19fcd2b3bab4374b7e8e6968034cb8e85e
ASVIEST <aaaaaabbbbbbccscc@gmail.com>
2023-08-29 15:02:34 +0300

    Update deferred_procs.nim

--- compiler/deferred_procs.nim
+++ compiler/deferred_procs.nim
@@ -1,8 +1,14 @@
-import ast, semdata, renderer, lookups, types
+import ast, semdata, lookups, types
 
 proc procBodyDeferredAux(c: PContext, n: PNode, res: var bool, affectsResult, isLast = false): bool =
   # very simple deferred proc checker
   # it cheaper than semProcBody(it calls semExpr)
+  proc isDeferredProc(n: PNode): bool =
+    discard procBodyDeferredAux(c, n[bodyPos], result, true)
+
+  template earlyExit: untyped =
+    return true
+
   var i = 1
   case n.kind:
     of nkStmtList:
@@ -10,7 +16,7 @@ proc procBodyDeferredAux(c: PContext, n: PNode, res: var bool, affectsResult, is
         if procBodyDeferredAux(c, e, res, affectsResult, i == n.len - 1):
           break
     of nkAsgn:
-      if $n[0] == "result":
+      if n[0].ident.s == "result":
         # result = ...
         discard procBodyDeferredAux(c, n[1], res, true)
     
@@ -21,18 +27,20 @@ proc procBodyDeferredAux(c: PContext, n: PNode, res: var bool, affectsResult, is
         ident = ident[0]
       var s = lookUp(c, ident)# it can produce error for undeclarated
 
-      if s.ast != nil and s.ast.kind in {nkProcDef, nkFuncDef}:
-        let gp = s.ast[genericParamsPos]
+      if (let procDef = s.ast; procDef) != nil and procDef.kind in {nkProcDef, nkFuncDef}:
+        let gp = procDef[genericParamsPos]
         let typ = s.getReturnType
+        let genericOrDeferred =
+          gp.isGenericParams or
+          (typ.kind == tyAnything and isDeferredProc(procDef))
 
-        if (gp.isGenericParams or typ.kind == tyAnything) and n[0].kind == nkIdent and (affectsResult or isLast):
-          # genericProc()
+        if genericOrDeferred and n[0].kind == nkIdent and (affectsResult or isLast):
+          # generic or deferred proc
           res = true
                 
         elif typ != nil and not isLast:
           res = false
-          result = true
-          return
+          earlyExit
     else:
       discard
 
